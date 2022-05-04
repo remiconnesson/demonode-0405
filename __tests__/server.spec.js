c8226710139a59e0caf6d7b5787a787c41eb7e7a
Rémi Connesson <remiconnesson@gmail.com>
2022-05-04 16:32:33 +0200

    Deactivate test suite.

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -9,7 +9,7 @@ const mapToObj = (m) => {
 };
 
 describe("Mon API CRUD", () => {
-  it("GET /api/names retourne JSON de la database", async () => {
+  it.skip("GET /api/names retourne JSON de la database", async () => {
     const res = await request(server)
       .get("/api/names")
       .expect(200)
@@ -17,7 +17,7 @@ describe("Mon API CRUD", () => {
     expect(JSON.parse(res.text)).toMatchObject(mapToObj(db.memoryDb));
   });
 
-  it("POST /api/names doit créer un nouvel objet en BDD et le retourner", async () => {
+  it.skip("POST /api/names doit créer un nouvel objet en BDD et le retourner", async () => {
     let insertion = { name: "Insertion" }
     let id = db.id
     const res = await request(server)
@@ -30,7 +30,7 @@ describe("Mon API CRUD", () => {
   });
 
 
-  it("PUT /api/name/:id modifie l'objet correspondant en DB", async () => {
+  it.skip("PUT /api/name/:id modifie l'objet correspondant en DB", async () => {
     let modification = { name: "Modified" }
     const res = await request(server)
       .put("/api/name/1")
@@ -39,7 +39,7 @@ describe("Mon API CRUD", () => {
     expect(modification).toMatchObject(db.memoryDb.get(1));
   });
 
-  it("GET /api/name/:id retourne le JSON de l'objet correspondant en DB", async () => {
+  it.skip("GET /api/name/:id retourne le JSON de l'objet correspondant en DB", async () => {
     const res = await request(server)
       .get("/api/name/1")
         .expect(200)
