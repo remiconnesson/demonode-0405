d9f34ce9a15429a2b3892b33a4d3bcf5bdac2d0d
Rémi Connesson <remiconnesson@gmail.com>
2022-05-04 14:27:50 +0200

    Create an in memory db module.

---
+++ db.js
@@ -0,0 +1,13 @@
+db = {
+  memoryDb: new Map();
+  id:0;
+
+}
+
+
+db['memoryDb'].set(db['id']++, { nom: "Alice" }) // voici comment set une nouvelle entrée.
+
+db['memoryDb'].set(db['id']++, { nom: "Bob" })
+db['memoryDb'].set(db['id']++, { nom: "Charlie" })
+
+module.exports = db
